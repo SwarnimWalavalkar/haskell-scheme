e586960b76626de0317950ce32c79f37138cf2c5
Swarnim Walavalkar <swarnim.walavalkar@gmail.com>
2022-01-16 15:09:41 +0530

    implement REPL

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,6 @@
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment ( getArgs )
+import System.IO
 import Control.Monad
 import Control.Monad.Except
 import Foreign.C (throwErrnoIfRetry)
@@ -112,6 +113,25 @@ unpackBool :: LispVal -> ThrowsError Bool
 unpackBool (Bool b) = return b
 unpackBool notBool  = throwError $ TypeMismatch "boolean" notBool
 
+flushStr :: String -> IO()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString expr = return $ extractValue $ trapError (liftM show $ readExpr expr >>= eval)
+
+evalAndPrint :: String -> IO()
+evalAndPrint expr = evalString expr >>= putStrLn
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ())  -> m ()
+until_ pred prompt action = do
+  result <- prompt
+  if pred result
+    then return ()
+    else action result >> until_ pred prompt action
+
 spaces :: Parser ()
 spaces = skipMany1 space
 
@@ -181,8 +201,12 @@ apply func args = maybe (throwError $ NotFunction "Unrecognized primitive functi
                         ($ args)
                         (lookup func primitives)
 
+runRepl :: IO ()
+runRepl = until_ (== "quit") (readPrompt "Lisp>>> ") evalAndPrint
+
 main :: IO ()
-main =  do
-  args <- getArgs 
-  evaled <- return $ liftM show $ readExpr (args !! 0) >>= eval
-  putStrLn $ extractValue $ trapError evaled
+main = do args <- getArgs
+          case length args of
+               0 -> runRepl
+               1 -> evalAndPrint $ args !! 0
+               otherwise -> putStrLn "Program takes only 0 or 1 argument"
